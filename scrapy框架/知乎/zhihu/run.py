77fca123e45daaf0d24c61f13b99bc6ccb1ab8fd
PaperStrange <Dr_in_Dream@163.com>
2020-02-27 20:14:31 +0800

    REFACTOR: modify and close proxy module

--- scrapy框架/知乎/zhihu/run.py
+++ scrapy框架/知乎/zhihu/run.py
@@ -1,5 +1,7 @@
 # from scrapy.cmdline import execute
 from scrapy import cmdline
-
+import os
 
 cmdline.execute('scrapy crawl yiqing_zhihu'.split())
+# TODO 2020/02/27-1:35: deal with the stop time
+# os.system("shutdown -s -t  60 ")
