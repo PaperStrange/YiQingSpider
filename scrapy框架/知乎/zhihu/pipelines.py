e02734f094f9dd530fef830afbc6a4dbfc2b99a9
PaperStrange <Dr_in_Dream@163.com>
2020-03-02 23:34:03 +0800

    REFACTOR: keep the same style of output table column

--- scrapy框架/知乎/zhihu/pipelines.py
+++ scrapy框架/知乎/zhihu/pipelines.py
@@ -10,11 +10,11 @@ import datetime
 
 
 CURRENT_DATE = datetime.datetime.now()
-FILENAME = '知乎_{}-{}-{}.csv'.format(str(CURRENT_DATE.year),str(CURRENT_DATE.month),str(CURRENT_DATE.day))
+FILENAME = '知乎_{}-{}-{}.csv'.format(str(CURRENT_DATE.year), str(CURRENT_DATE.month), str(CURRENT_DATE.day))
 
 class ZhihuPipeline(object):
     def process_item(self, item, spider):
-        if item['time'] == None:
+        if item['time'] is None:
             pass
         else:
             with open(FILENAME, 'a+', newline='', encoding='utf_8') as f:
@@ -22,59 +22,105 @@ class ZhihuPipeline(object):
                     'source', 'title', 'url', 'time', 'author', 'author_url', 'description'
                     ]
                 writer = csv.DictWriter(f, fieldnames=fieldnames)
-                if '2020' in item['time']:
-                    try:
-                        year = re.findall('2020-(.*?)-',item['time'])[0]
-                    except:
-                        year = '01'
-                    day = int(re.findall('2020-\d\d-(\d\d)',item['time'])[0])
-                    if year == '01':
-                        if day >= 23:
+                if 'question' not in item['url']:
+                    
+                    if '2020' in item['time']:
+                        try:
+                            year = re.findall('2020-(.*?)-', item['time'])[0]
+                        except:
+                            year = '01'
+                        day = int(re.findall('2020-\d\d-(\d\d)', item['time'])[0])
+                        if year == '01':
+                            if day >= 23:
+                                part = item['time'].split(" ")
+                                if len(part) == 2:
+                                    if len(part[0]) > len(part[1]):
+                                        shorttime = part[0]
+                                    else:
+                                        shorttime = part[1]
+                                    writer.writerow(
+                                        {
+                                            'source': item['source'], 
+                                            'title': item['title'],
+                                            'author': item['author'],
+                                            'time': shorttime,
+                                            'url': item['url'],
+                                            'description': item['description'],
+                                            'author_url': item['author_url'],
+                                            'importance': "" 
+                                        }
+                                    )                                           
+                                else:
+                                    print(item['time'])
+                                    writer.writerow(
+                                        {
+                                            'source': item['source'], 
+                                            'title': item['title'],
+                                            'author': item['author'],
+                                            'time': item['time'],
+                                            'url': item['url'],
+                                            'description': item['description'],
+                                            'author_url': item['author_url'],
+                                            'importance': "" 
+                                        }
+                                    ) 
+                        else:
+                            part = item['time'].split(" ")
+                            if len(part) == 2:
+                                if len(part[0]) > len(part[1]):
+                                    shorttime = part[0]
+                                else:
+                                    shorttime = part[1] 
                                 writer.writerow(
                                     {
                                         'source': item['source'], 
                                         'title': item['title'],
-                                        'url': item['url'],
-                                        'time': item['time'], 
                                         'author': item['author'],
-                                        'author_url':item['author_url'],
-                                        'description':item['description']
+                                        'time': shorttime,
+                                        'url': item['url'],
+                                        'description': item['description'],
+                                        'author_url': item['author_url'],
+                                        'importance': "" 
                                     }
-                                )
-                    else:
-                        writer.writerow(
-                            {
-                                'source': item['source'], 
-                                'title': item['title'], 
-                                'url': item['url'], 
-                                'time': item['time'],
-                                'author': item['author'], 
-                                'author_url': item['author_url'],
-                                'description': item['description']
-                            }
-                        )
-                elif '昨天' in item['time']:
+                                )                                           
+                            else:
+                                print(item['time'])
+                                writer.writerow(
+                                    {
+                                        'source': item['source'], 
+                                        'title': item['title'],
+                                        'author': item['author'],
+                                        'time': item['time'],
+                                        'url': item['url'],
+                                        'description': item['description'],
+                                        'author_url': item['author_url'],
+                                        'importance': "" 
+                                    }
+                                )                                                        
+                    elif '昨天' in item['time']:
                         writer.writerow(
                             {
                                 'source': item['source'], 
                                 'title': item['title'],
-                                'url': item['url'],
-                                'time': '{}-{}-{}'.format(str(CURRENT_DATE.year),str(CURRENT_DATE.month),str(CURRENT_DATE.day-1)), 
                                 'author': item['author'],
+                                'time': '{}-{}-{}'.format(str(CURRENT_DATE.year), str(CURRENT_DATE.month), str(CURRENT_DATE.day-1)),
+                                'url': item['url'],
+                                'description': item['description'],
                                 'author_url': item['author_url'],
-                                'description':item['description']
+                                'importance': "" 
                             }
-                        )
-                elif len(item['time']) < 10:
+                        )  
+                    elif len(item['time']) < 10:
                         writer.writerow(
                             {
                                 'source': item['source'], 
-                                'title': item['title'], 
-                                'url': item['url'], 
-                                'time': '{}-{}-{}'.format(str(CURRENT_DATE.year),str(CURRENT_DATE.month),str(CURRENT_DATE.day)),
-                                'author': item['author'], 
+                                'title': item['title'],
+                                'author': item['author'],
+                                'time': '{}-{}-{}'.format(str(CURRENT_DATE.year), str(CURRENT_DATE.month), str(CURRENT_DATE.day)),
+                                'url': item['url'],
+                                'description': item['description'],
                                 'author_url': item['author_url'],
-                                'description': item['description']
+                                'importance': "" 
                             }
-                        )
+                        ) 
         return item
